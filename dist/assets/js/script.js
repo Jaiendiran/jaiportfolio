49f48482d14c4b82bbf7cf7a48ec98572533ee57
Jaiendiran <jaiendiran2494@gmail.com>
2018-12-16 22:32:51 +0530

    js commit

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -1,4 +1,4 @@
-<script>
+
 $(function () {
   var nua = navigator.userAgent
   var isAndroid = (nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1 && nua.indexOf('Chrome') === -1)
@@ -6,4 +6,12 @@ $(function () {
     $('select.form-control').removeClass('form-control').css('width', '100%')
   }
 })
-</script>
+
+
+
+var nua = navigator.userAgent;
+var is_android = ((nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1) && !(nua.indexOf('Chrome') > -1));
+if(is_android) {
+        $('select.form-control').removeClass('form-control').css('width', '100%');
+
+}
